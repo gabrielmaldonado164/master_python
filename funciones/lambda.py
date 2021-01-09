af59eb92475c4e6485e70863df773406b1ee283c
gabrielmaldonado164 <gabriel.hernan.maldonado@gmail.com>
2021-01-08 21:59:04 -0300

    listas

---
+++ funciones/lambda.py
@@ -0,0 +1,13 @@
+"""
+Funciones lambda:
+-Es una funcion anonima que no tiene nombre  son par atareas pqueñas, no hace falta indicar la palabra 'def'.
+Su definicion es solo es en una solo linea
+
+"""
+
+#Syntax
+#nombre_variable : labmda parametro : operacion
+
+decir_year = lambda  year: f'El año es {year}'
+
+print(decir_year(2021))
---
+++ listas/main.py
@@ -0,0 +1,25 @@
+"""
+LISTAS(arrays)
+- Son colecciones o conjuntos de datos/valores, bajo un unico nombre
+- Para acceder a esos valores podemos usar un indice numerico
+- Una variable que guarde un conjunto de valores
+"""
+
+#variable
+pelicula = 'Avenger'
+
+#lista
+peliculas = ['Avenger', 'Iron man', 'Hulk']
+print(peliculas)
+
+#otra maneras de generar listas
+
+cantantes = list(('rauw alejandro' , 'wisin y yandel', 'eminem'))
+print(cantantes)
+
+
+
+#con rango
+
+year = list(range(2020,2035))
+print(year)
