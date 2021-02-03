90328be9810898fc3fb2bace4ae4b6fc592f74df
Alexei <alexei97531@gmail.com>
2021-02-03 14:06:41 -0300

    Te falto declarar la variable 'conexion' al principio de todo xD y acordate de instalar bien los modulos e_e

--- proyectos/abm/bd.py
+++ proyectos/abm/bd.py
@@ -1,6 +1,8 @@
 import pymysql
 
 class Database():
+    conexion = None
+
     def conectar(self):
         try:
             self.conexion = pymysql.connect(host='ca8.toservers.com',
@@ -13,3 +15,5 @@ class Database():
             print(f'Lo sinto, hubo un error en la conexion: {e}')
     
    
+db = Database()
+db.conectar()
